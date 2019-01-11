a08bc0e78fc0f5915a7d81e2525f489614701703
Andrew Hill <andrewh.5299@gmail.com>
2019-01-11 17:34:41 -0600

    Initial commit of default libgdx project

---
+++ desktop/src/com/andrewkhill/coolgame/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.andrewkhill.coolgame.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.andrewkhill.coolgame.Game
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(Game(), config)
+    }
+}
