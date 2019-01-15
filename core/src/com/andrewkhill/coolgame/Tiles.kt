e3bce19de5bfe0d45f18fa86892c500a2a4b2103
Andrew Hill <andrewh.5299@gmail.com>
2019-01-15 16:28:44 -0600

    Add Tiles.fromSymbol()

--- core/src/com/andrewkhill/coolgame/Tiles.kt
+++ core/src/com/andrewkhill/coolgame/Tiles.kt
@@ -10,4 +10,19 @@ package com.andrewkhill.coolgame
  * @param asset The image file that [MapDisplayer] should use to represent the tile.
  */
 enum class Tiles (val symbol : String, val asset : String) {
+
 }
+
+/**
+ * @param symbol The symbolic representation of the tile used in [TileMap]
+ * @return The [Tiles] enum object for the tile.
+ */
+fun fromSymbol(symbol : String) : Tiles? {
+    val map = Tiles.values().associateBy(Tiles::symbol)
+    return map.get(symbol)
+}
+/**
+ * for char in map {
+ *   Texture(Tiles.getTile(char))
+ * }
+ */
