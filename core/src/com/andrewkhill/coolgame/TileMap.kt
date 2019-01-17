8720d02f624e6947d2d44794669b013ef059cc6f
David Goforth <goforthd@dlstx.org>
2019-01-17 16:09:24 -0600

    Added basic function for changing tiles.

--- core/src/com/andrewkhill/coolgame/TileMap.kt
+++ core/src/com/andrewkhill/coolgame/TileMap.kt
@@ -8,6 +8,14 @@ package com.andrewkhill.coolgame
  * @property map The string representation of the map.
  * @constructor Creates a map from the given string representation.
  */
-class TileMap (val map : String){
+class TileMap (val map : Array<String>) {
+
+    fun ChangeTileType (x : Int, y : Int, newType : Char) {
+
+        var desiredRow = map[y].toCharArray()
+        desiredRow.set(x, newType)
+        map[y] = desiredRow.joinToString("")
+
+    }
 
 }
