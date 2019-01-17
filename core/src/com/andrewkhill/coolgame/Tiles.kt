060770e3fcf22ec2ba5d5d5b9f4f35cb9dd87cb2
Andrew Hill <andrewh.5299@gmail.com>
2019-01-17 12:25:15 -0600

    Add dirt and stone assets

--- core/src/com/andrewkhill/coolgame/Tiles.kt
+++ core/src/com/andrewkhill/coolgame/Tiles.kt
@@ -9,23 +9,19 @@ package com.andrewkhill.coolgame
  * @param symbol The symbolic representation of the tile used in [TileMap]
  * @param asset The image file that [MapDisplayer] should use to represent the tile.
  */
-enum class Tiles (val symbol : String, val asset : String) {
-    GRASS("g", "tiles/grass.png");
+enum class Tiles (val symbol : Char, val asset : String) {
+    GRASS('g', "tiles/grass.png"),
+    STONE('s', "tiles/stone.png"),
+    DIRT('d', "tiles/dirt.png");
 
     companion object {
         /**
          * @param symbol The symbolic representation of the tile used in [TileMap]
          * @return The [Tiles] enum object for the tile.
          */
-        fun fromSymbol(symbol: String): Tiles? {
+        fun fromSymbol(symbol: Char): Tiles? {
             val map = Tiles.values().associateBy(Tiles::symbol)
             return map.get(symbol)
         }
     }
 }
-
-/**
- * for char in map {
- *   Texture(Tiles.getTile(char))
- * }
- */
